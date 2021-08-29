c7c389351ad138a203c50f0733f55072c1fcde28
gsainfoteam <jaehong21@gm.gist.ac.kr>
2021-08-29 19:25:13 +0900

    page

--- App.js
+++ App.js
@@ -2,6 +2,19 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Schedule from './pages/Schedule';
+import {SafeAreaView, Text} from 'react-native';
+
+const ScheduleScreen = () => {
+  return <Schedule />;
+};
+
+const LectureScreen = () => {
+  return <Schedule />;
+};
+
+const ProfileScreen = () => {
+  return <Schedule />;
+};
 
 const App = () => {
   return <Schedule />;
